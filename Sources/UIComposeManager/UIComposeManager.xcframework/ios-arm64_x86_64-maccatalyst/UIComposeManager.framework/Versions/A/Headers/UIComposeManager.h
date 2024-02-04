873ca7407cef05dcc0cea22523b965bd79e30f73
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-03 19:31:34 -0500

    framework added

---
+++ Sources/UIComposeManager/UIComposeManager.xcframework/ios-arm64_x86_64-maccatalyst/UIComposeManager.framework/Versions/A/Headers/UIComposeManager.h
@@ -0,0 +1,18 @@
+//
+//  UIComposeManager.h
+//  UIComposeManager
+//
+//  Created by Sukhaman Singh on 2/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIComposeManager.
+FOUNDATION_EXPORT double UIComposeManagerVersionNumber;
+
+//! Project version string for UIComposeManager.
+FOUNDATION_EXPORT const unsigned char UIComposeManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComposeManager/PublicHeader.h>
+
+
