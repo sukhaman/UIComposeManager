6e457841ccce0cabf25a04303e3607222ae1daf2
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-03 19:44:01 -0500

    Updated

---
+++ Sources/UIComposeManager/SecondaryLabel.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sukhaman Singh on 2/3/24.
+//
+
+import Foundation
+
+public struct SDK {
+    
+    public static func typeToWork() {
+        print("Work")
+    }
+}
