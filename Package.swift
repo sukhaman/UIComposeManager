c5c6d6f0a6783f0531954155acaa34b1ecd55670
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-03 22:41:44 -0500

    added dependecies

--- Package.swift
+++ Package.swift
@@ -5,12 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "UIComposeManager",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "UIComposeManager",
-            targets: ["UIComposeManager"]),
-    ],
+    dependencies: [
+            .package(url: "https://github.com/sukhaman/UIComposeManager-build.git", from: "0.0.2"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
